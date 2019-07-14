852704ec0b9d3df1eb32b32dc7d4b4cf8bba45c4
Praveen T <praveentiru@users.noreply.github.com>
2019-07-14 19:43:37 +0530

    Implemented drop 1 key Excel text functions

--- efp.go
+++ efp.go
@@ -47,14 +47,82 @@ var excelText = gval.NewLanguage(
 		bStr := toString(b)
 		return Exact(aStr, bStr)
 	}),
-	gval.Function("FIND", func(src, fnd interface{}, num ...float64) int {
+	gval.Function("FIND", func(src, fnd interface{}, num ...float64) float64 {
 		srcStr := toString(src)
 		fndStr := toString(fnd)
 		startPos := 1
 		if len(num) > 0 {
 			startPos = int(num[0])
 		}
-		return Find(srcStr, fndStr, startPos)
+		return float64(Find(srcStr, fndStr, startPos))
+	}),
+	gval.Function("LEFT", func(s interface{}, num ...float64) string {
+		str := toString(s)
+		l := 1
+		if len(num) > 0 {
+			l = int(num[0])
+		}
+		return Left(str, l)
+	}),
+	gval.Function("LEN", func(s interface{}) float64 {
+		str := toString(s)
+		return float64(Len(str))
+	}),
+	gval.Function("LOWER", func(s interface{}) string {
+		str := toString(s)
+		return Lower(str)
+	}),
+	gval.Function("MID", func(s interface{}, strt, num float64) string {
+		str := toString(s)
+		return Mid(str, int(strt), int(num))
+	}),
+	gval.Function("PROPER", func(a interface{}) string {
+		str := toString(a)
+		return Proper(str)
+	}),
+	gval.Function("REPLACE", func(a interface{}, strt, num float64, b interface{}) string {
+		aStr := toString(a)
+		bStr := toString(b)
+		return Replace(aStr, int(strt), int(num), bStr)
+	}),
+	gval.Function("REPT", func(s interface{}, num float64) string {
+		str := toString(s)
+		return Rept(str, int(num))
+	}),
+	gval.Function("RIGHT", func(s interface{}, num ...float64) string {
+		str := toString(s)
+		l := 1
+		if len(num) > 0 {
+			l = int(num[0])
+		}
+		return Right(str, l)
+	}),
+	gval.Function("SEARCH", func(fnd, src interface{}, strt ...float64) float64 {
+		fndStr := toString(fnd)
+		srcStr := toString(src)
+		l := 1
+		if len(strt) > 0 {
+			l = int(strt[0])
+		}
+		return float64(Search(fndStr, srcStr, l))
+	}),
+	gval.Function("SUBSTITUTE", func(src, old, new interface{}, num ...float64) string {
+		srcStr := toString(src)
+		oldStr := toString(old)
+		newStr := toString(new)
+		n := 0
+		if len(num) > 0 {
+			n = int(num[0])
+		}
+		return Substitute(srcStr, oldStr, newStr, n)
+	}),
+	gval.Function("TRIM", func(s interface{}) string {
+		str := toString(s)
+		return Trim(str)
+	}),
+	gval.Function("UPPER", func(s interface{}) string {
+		str := toString(s)
+		return Upper(str)
 	}),
 )
 
--- efp_test.go
+++ efp_test.go
@@ -1,12 +1,16 @@
 package efp_test
 
 import (
+	"context"
+	"reflect"
 	"strings"
 	"testing"
 
 	"github.com/praveentiru/efp"
 )
 
+// TODO: Add failure cases to ensure that parser throws errors when parameters are incorrect
+// TODO: Add test cases for nested function calls
 func TestParse(t *testing.T) {
 	tt := []struct {
 		name string
@@ -17,9 +21,23 @@ func TestParse(t *testing.T) {
 		{"CONCAT with strings and float", `CONCAT("Hello ", 3.1416)`, "Hello 3.1416"},
 		{"CONCAT with strings and int", `CONCAT("Hello ", 42)`, "Hello 42"},
 		{"EXACT function", `EXACT("Hello", "Hello")`, true},
-		{"FIND function with start position", `FIND("Hello", "l", 2)`, 3},
-		{"FIND function without start position", `FIND("Hello", "l")`, 3},
-		{"FIND function in lower case", `find("Hello", "l")`, 3},
+		{"FIND function with start position", `FIND("l", "Hello", 2)`, 3.0},
+		{"FIND function without start position", `FIND("l", "Hello")`, 3.0},
+		{"FIND function in lower case", `FIND("l", "Hello")`, 3.0},
+		{"LEFT with string and no num chars", `LEFT("Hello World")`, "H"},
+		{"LEFT with string and with num chars", `LEFT("Hello World", 5)`, "Hello"},
+		{"LEFT with number input in place of text", `LEFT(3.1416, 3)`, "3.1"},
+		{"LEN for string", `LEN("Hello World")`, 11.0},
+		{"LOWER with string", `LOWER("HeLLo World")`, "hello world"},
+		{"MID with string", `MID("Hello World", 3, 3)`, "llo"},
+		{"PROPER with string", `PROPER("HEllO wORLd")`, "Hello World"},
+		{"REPLACE with string", `REPLACE("Hello World", 7, 5, "India")`, "Hello India"},
+		{"REPT with string", `REPT("Hell", 4)`, "HellHellHellHell"},
+		{"RIGHT with string and num", `RIGHT("Hell", 2)`, "ll"},
+		{"SEARCH with mis-match case", `SEARCH("LL", "Hello World")`, 3.0},
+		{"SUBSTITUTE with string", `SUBSTITUTE("Oink Oink Oink", "ink", "inky", 2)`, "Oink Oinky Oink"},
+		{"TRIM with spaces", `TRIM("    Hello    World    ")`, "Hello World"},
+		{"UPPER with inconsistent csae", `UPPER("Hello India")`, "HELLO INDIA"},
 	}
 	var errCnt int
 	for _, tu := range tt {
@@ -27,6 +45,73 @@ func TestParse(t *testing.T) {
 		if err != nil {
 			t.Logf("Test Case: %v, Parse Error: %v", tu.name, err.Error())
 			errCnt++
+		}
+	}
+	if errCnt > 0 {
+		t.Errorf("Failed %v of %v cases", errCnt, len(tt))
+	}
+}
+
+func TestEvaluate(t *testing.T) {
+	tt := []struct {
+		name string
+		exp  string
+		out  interface{}
+	}{
+		{"CONCAT with strings only", `CONCAT("Hello ", "World")`, "Hello World"},
+		{"CONCAT with strings and float", `CONCAT("Hello ", 3.1416)`, "Hello 3.1416"},
+		{"CONCAT with strings and int", `CONCAT("Hello ", 42)`, "Hello 42"},
+		{"EXACT function", `EXACT("Hello", "Hello")`, true},
+		{"FIND function with start position", `FIND("l", "Hello", 2)`, 3.0},
+		{"FIND function without start position", `FIND("l", "Hello")`, 3.0},
+		{"FIND function in lower case", `FIND("l", "Hello")`, 3.0},
+		{"LEFT with string and no num chars", `LEFT("Hello World")`, "H"},
+		{"LEFT with string and with num chars", `LEFT("Hello World", 5)`, "Hello"},
+		{"LEFT with number input in place of text", `LEFT(3.1416, 3)`, "3.1"},
+		{"LEN for string", `LEN("Hello World")`, 11.0},
+		{"LOWER with string", `LOWER("HeLLo World")`, "hello world"},
+		{"MID with string", `MID("Hello World", 3, 3)`, "llo"},
+		{"PROPER with string", `PROPER("HEllO wORLd")`, "Hello World"},
+		{"REPLACE with string", `REPLACE("Hello World", 7, 5, "India")`, "Hello India"},
+		{"REPT with string", `REPT("Hell", 4)`, "HellHellHellHell"},
+		{"RIGHT with string and num", `RIGHT("Hell", 2)`, "ll"},
+		{"SEARCH with mis-match case", `SEARCH("LL", "Hello World")`, 3.0},
+		{"SUBSTITUTE with string", `SUBSTITUTE("Oink Oink Oink", "ink", "inky", 2)`, "Oink Oinky Oink"},
+		{"TRIM with spaces", `TRIM("    Hello    World    ")`, "Hello World"},
+		{"UPPER with inconsistent csae", `UPPER("Hello India")`, "HELLO INDIA"},
+	}
+	var errCnt int
+	for _, tu := range tt {
+		eval, err := efp.Parse(strings.NewReader(tu.exp))
+		if err != nil {
+			t.Logf("Test Case: %v, Parse Error: %v", tu.name, err.Error())
+			errCnt++
+			continue
+		}
+		if eval == nil {
+			t.Logf("Test Case: %v, No Parse error but Evaluable is nil", tu.name)
+			errCnt++
+			continue
+		}
+		switch v := reflect.ValueOf(tu.out); v.Kind() {
+		case reflect.String:
+			str, _ := eval.EvalString(context.Background(), nil)
+			if strings.Compare(str, v.String()) != 0 {
+				t.Logf("Test Case: %v, Expected: %v, Got: %v", tu.name, tu.out, str)
+				errCnt++
+			}
+		case reflect.Float64:
+			fl, _ := eval.EvalFloat64(context.Background(), nil)
+			if int(fl) != int(v.Float()) {
+				t.Logf("Test Case: %v, Expected: %v, Got: %v", tu.name, tu.out, fl)
+				errCnt++
+			}
+		case reflect.Bool:
+			b, _ := eval.EvalBool(context.Background(), nil)
+			if b != v.Bool() {
+				t.Logf("Test Case: %v, Expected: %v, Got: %v", tu.name, tu.out, b)
+				errCnt++
+			}
 		}
 	}
 	if errCnt > 0 {
