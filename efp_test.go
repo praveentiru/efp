4983b990953b40b0873b45c2d38dec6db48e5cf7
Praveen T <praveentiru@users.noreply.github.com>
2019-07-18 14:58:46 +0530

    Added benchmark

--- efp_test.go
+++ efp_test.go
@@ -241,3 +241,45 @@ func TestParameterExpressions(t *testing.T) {
 		t.Errorf("Failed %v of %v cases", errCnt, len(tt))
 	}
 }
+
+func BenchmarkParse(b *testing.B) {
+	tt := []struct {
+		name string
+		exp  string
+		out  interface{}
+	}{
+		{"CONCAT with strings only", `CONCAT("Hello ", "World")`, "Hello World"},
+		{"CONCAT with strings and float", `CONCAT("Hello ", 3.1416)`, "Hello 3.1416"},
+		{"CONCAT with strings and int", `CONCAT("Hello ", 42)`, "Hello 42"},
+		{"EXACT function", `EXACT("Hello", "Hello")`, true},
+		{"FIND function with start position", `FIND("l", "Hello", 2)`, 3.0},
+		{"FIND function without start position", `FIND("l", "Hello")`, 3.0},
+		{"LEFT with string and no num chars", `LEFT("Hello World")`, "H"},
+		{"LEFT with string and with num chars", `LEFT("Hello World", 5)`, "Hello"},
+		{"LEFT with number input in place of text", `LEFT(3.1416, 3)`, "3.1"},
+		{"LEN for string", `LEN("Hello World")`, 11.0},
+		{"LOWER with string", `LOWER("HeLLo World")`, "hello world"},
+		{"MID with string", `MID("Hello World", 3, 3)`, "llo"},
+		{"PROPER with string", `PROPER("HEllO wORLd")`, "Hello World"},
+		{"REPLACE with string", `REPLACE("Hello World", 7, 5, "India")`, "Hello India"},
+		{"REPT with string", `REPT("Hell", 4)`, "HellHellHellHell"},
+		{"RIGHT with string and num", `RIGHT("Hell", 2)`, "ll"},
+		{"SEARCH with mis-match case", `SEARCH("LL", "Hello World")`, 3.0},
+		{"SUBSTITUTE with string", `SUBSTITUTE("Oink Oink Oink", "ink", "inky", 2)`, "Oink Oinky Oink"},
+		{"TRIM with spaces", `TRIM("    Hello    World    ")`, "Hello World"},
+		{"UPPER with inconsistent csae", `UPPER("Hello India")`, "HELLO INDIA"},
+	}
+	var errCnt int
+	b.StartTimer()
+	for _, tu := range tt {
+		_, err := efp.Parse(strings.NewReader(tu.exp))
+		if err != nil {
+			b.Logf("Test Case: %v, Parse Error: %v", tu.name, err.Error())
+			errCnt++
+		}
+	}
+	b.StopTimer()
+	if errCnt > 0 {
+		b.Errorf("Failed %v of %v cases", errCnt, len(tt))
+	}
+}
